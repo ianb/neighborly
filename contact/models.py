352073e2ac73b2045f7a597543ce8069ce90a129
Ian Bicking <ianb@colorstudy.com>
2009-12-12 15:04:33 -0600

    Some GeoDjango tweaks

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,1 @@
-from django.db import models
-
-# Create your models here.
+from django.contrib.gis.db import models
--- contact/views.py
+++ contact/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+from django.http import HttpResponse
+
+def home(req):
+    return HttpResponse('homepage')
+
+def login(req):
+    return HttpResponse('login')
